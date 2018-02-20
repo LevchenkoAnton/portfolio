8dfb1e0c6aa6fd88f74a749b696b9bcb714521f1
Anton Levchenko <levchik03021998@gmail.com>
2018-02-20 17:29:33 +0200

    auto scroll to all blocks was done

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
-var burgerButton = document.querySelector(".header__open-nav");
-var mainNav = document.querySelector(".main-nav");
+$(document).ready(function(){
 
-burgerButton.addEventListener('click', function (event) {
-    event.preventDefault();
-    mainNav.classList.toggle("main-nav--closed");
+    $(".header__open-nav").on("click", function (event) {
+        event.preventDefault();
+        $(".main-nav").toggleClass("main-nav--closed");
+    });
+
+    $(".main-nav__item").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top - 65;
+        $('body,html').animate({scrollTop: top}, 1200);
+    });
+
+    $(".logo").on("click", function (event) {
+        event.preventDefault();
+        $('body,html').animate({scrollTop: 0}, 1200);
+    });
 });
+
 
