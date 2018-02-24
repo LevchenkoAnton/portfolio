f22d10ee5fad278d6a7850b1216f22f853bb4b0b
Anton Levchenko <levchik03021998@gmail.com>
2018-02-24 18:41:50 +0200

    animate for btn in main block was done

--- js/main.js
+++ js/main.js
@@ -9,13 +9,21 @@ $(document).ready(function(){
         event.preventDefault();
         var id  = $(this).attr('href'),
         top = $(id).offset().top - 65;
-        $('body,html').animate({scrollTop: top}, 1200);
+        $("body,html").animate({scrollTop: top}, 1200);
+        $(".main-nav").addClass("main-nav--closed");
     });
 
     $(".logo").on("click", function (event) {
         event.preventDefault();
-        $('body,html').animate({scrollTop: 0}, 1200);
+        $("body,html").animate({scrollTop: 0}, 1200);
     });
+
+    $(".main__btn").on("click", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top - 65;
+        $("body,html").animate({scrollTop: top}, 1200);
+    })
 });
 
 
